16f039c7f682e4da41f852872fc7c2e162cc252b
skywalker009 <113121760+skywalker009@users.noreply.github.com>
2024-07-21 23:50:26 +0530

    updated code

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,27 @@
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect } from 'react';
+import api from './api';
+
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+  const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const setToken = async () => {
+      if (isAuthenticated) {
+        const token = await getAccessTokenSilently();
+        localStorage.setItem('authToken', token);
+      } else {
+        localStorage.removeItem('authToken');
+      }
+    };
+    setToken();
+  }, [isAuthenticated, getAccessTokenSilently]);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
